c54ee7a55c980bd4806ae66a9af48f92ad55b79b
Dev Vitor <160429477+Dev-Vitor-lev@users.noreply.github.com>
2024-08-13 18:12:01 -0300

    alt]

--- Refri/script.js
+++ Refri/script.js
@@ -5,17 +5,40 @@ let next = document.getElementById('next')
 let count = list.length
 let active = 0
 
-next.addEventListener('click',()=>{
-   let Actives = document.querySelector('.active')
-   Actives.classList.remove('active')
-
-   active = active >= count-1 ? 0 :active+1
-   list[active].classList.add('active')
+document.addEventListener('keydown', e =>{
+    if(e.key=='ArrowRight'){
+        let Actives = document.querySelector('.active')
+        Actives.classList.remove('active')
+     
+        active = active >= count-1 ? 0 :active+1
+        list[active].classList.add('active')
+    }
 })
-prev.addEventListener('click',()=>{
-    let Actives = document.querySelector('.active')
-    Actives.classList.remove('active')
-
-    active = active <= 0 ? count - 1 :active-1
-    list[active].classList.add('active')
+document.addEventListener('keydown', e =>{
+    if(e.key=='ArrowLeft'){
+        let Actives = document.querySelector('.active')
+        Actives.classList.remove('active')
+     
+        active = active <= 0? count-1 :active-1
+        list[active].classList.add('active')
+    }
 })
+
+next.addEventListener('click',() => {
+  
+        let Actives = document.querySelector('.active')
+        Actives.classList.remove('active')
+     
+        active = active >= count-1 ? 0 :active+1
+        list[active].classList.add('active')
+
+})
+prev.addEventListener('click', () =>{
+   
+        let Actives = document.querySelector('.active')
+        Actives.classList.remove('active')
+     
+        active = active <= 0? count-1 :active-1
+        list[active].classList.add('active')
+})
+
