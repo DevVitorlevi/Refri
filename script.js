f14170c777241276555eab9068c15a658ffde1b0
Dev Vitor <160429477+DevVitorlevi@users.noreply.github.com>
2024-08-24 02:06:06 -0300

    att

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let list = document.querySelectorAll('.item')
 let prev = document.getElementById('prev')
 let next = document.getElementById('next')
-let count = list.length
+let count = list.length //0,1,2
 let active = 0
 
 document.addEventListener('keydown', e =>{
-    if(e.key=='ArrowRight'){
+    if(e.key == 'ArrowRight'){
         let Actives = document.querySelector('.active')
         Actives.classList.remove('active')
      
-        active = active >= count-1 ? 0 :active+1
+        active = active >= count-1 ? 0 : active+1
         list[active].classList.add('active')
     }
 })
@@ -18,7 +18,7 @@ document.addEventListener('keydown', e =>{
         let Actives = document.querySelector('.active')
         Actives.classList.remove('active')
      
-        active = active     = 0? count-1 :active-1
+        active = active == 0 ? count-1 : active-1
         list[active].classList.add('active')
     }
 })
